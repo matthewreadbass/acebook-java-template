59da33329b3d70581aa8abac9c6a9b8ad6acd8c3
Patrick Chan <q_p18@hotmail.com>
2021-11-03 16:57:58 +0000

    Extract signUp, signIn, logOut methods into TestService

--- src/test/java/LogoutTest.java
+++ src/test/java/LogoutTest.java
@@ -1,14 +1,7 @@
-import com.github.javafaker.Faker;
 import com.makersacademy.acebook.Application;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -16,41 +9,19 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SpringBootTest(classes = Application.class)
 public class LogoutTest {
 
-    WebDriver driver;
-    Faker faker;
-    @Value("${spring.datasource.chromedriver}")
-    private String chromedriver;
+  private final TestService testService;
 
-    @Before
-    public void setup() {
-        System.setProperty("webdriver.chrome.driver", chromedriver);
-        driver = new ChromeDriver();
-        faker = new Faker();
-    }
+  public LogoutTest(TestService testService) {
+    this.testService = testService;
+  }
 
-    @After
-    public void tearDown() {
-        driver.close();
-    }
+  @Test
+  public void successfulLogoutDirectsToSignIn() {
+    String randomName = testService.signUp();
+    testService.signIn(randomName);
+    testService.logOut();
 
-    @Test
-    public void successfulLogoutDirectsToSignIn() {
-        driver.get("http://localhost:8080/users/new");
-        String randomName = faker.name().firstName();
-        driver.findElement(By.id("username")).sendKeys(randomName);
-        driver.findElement(By.id("password")).sendKeys("password");
-        driver.findElement(By.id("submit")).click();
-        
-        // TimeUnit.SECONDS.sleep(1);
-        // Thread.sleep(90000);
-        // driver.get("http://localhost:8080");
-        driver.findElement(By.id("username")).sendKeys(randomName);
-        driver.findElement(By.id("password")).sendKeys("password");
-        driver.findElement(By.className("btn")).click();
-
-        // driver.get("http://localhost:8080/posts");
-        driver.findElement(By.className("logout_btn")).click();
-        String title = driver.getTitle();
-        Assert.assertEquals("Please sign in", title);
-    }
+    String title = testService.driver.getTitle();
+    Assert.assertEquals("Please sign in", title);
+  }
 }
--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -1,15 +1,8 @@
-import com.github.javafaker.Faker;
-import com.makersacademy.acebook.Application;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
+
+import com.makersacademy.acebook.Application;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -17,30 +10,17 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SpringBootTest(classes = Application.class)
 public class SignUpTest {
 
-    WebDriver driver;
-    Faker faker;
-    @Value("${spring.datasource.chromedriver}")
-    private String chromedriver;
+  private final TestService testService;
 
-    @Before
-    public void setup() {
-        System.setProperty("webdriver.chrome.driver", chromedriver);
-        driver = new ChromeDriver();
-        faker = new Faker();
-    }
+  public SignUpTest(TestService testService) {
+    this.testService = testService;
+  }
 
-    @After
-    public void tearDown() {
-        driver.close();
-    }
-
-    @Test
-    public void successfulSignUpRedirectsToSignIn() {
-        driver.get("http://localhost:8080/users/new");
-        driver.findElement(By.id("username")).sendKeys(faker.name().firstName());
-        driver.findElement(By.id("password")).sendKeys("password");
-        driver.findElement(By.id("submit")).click();
-        String title = driver.getTitle();
-        Assert.assertEquals("Please sign in", title);
-    }
+  @Test
+  public void successfulSignUpRedirectsToSignIn() {
+    testService.signUp();
+    
+    String title = testService.driver.getTitle();
+    Assert.assertEquals("Please sign in", title);
+  }
 }
